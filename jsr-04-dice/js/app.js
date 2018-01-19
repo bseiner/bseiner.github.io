df9bb057cfba985a31e0a6bec012feb96537a8eb
bseiner <brseiner@gmail.com>
2018-01-18 20:37:01 -0500

    updating

--- jsr-04-dice/js/app.js
+++ jsr-04-dice/js/app.js
@@ -17,19 +17,16 @@ Check to see if the "Roll the Dice" button has been clicked; if it has, run the 
 */
 
 
-<script>
-
 document.getElementById('roll-dice').onclick = diceRoll();
 
 
 function diceRoll() = {
-	var random1 = Math.floor((Math.random()*6));
-	var random2 = Math.floor((Math.random()*6));
+	var random1 = Math.floor((Math.random()*6) +1);
+	var random2 = Math.floor((Math.random()*6) +1;
 	var firstDie  = "dice-" + random1;
 	var secondDie = "dice-" + random2;
 	document.getElementById("first-die").className = firstDie;
 	document.getElementById("second-die").className = secondDie;
 
 }
-</script>
 
