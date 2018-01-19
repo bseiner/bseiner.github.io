9e45c0cac27f237f522312b4a78dcfd6cad71907
bseiner <brseiner@gmail.com>
2018-01-18 21:17:21 -0500

    next commit

--- jsr-hw-4/js/index.js
+++ jsr-hw-4/js/index.js
@@ -4,16 +4,18 @@ var dogs = [];
 
 document.getElementById('formSubmit').onclick = orderPizza;
 
-function orderPizza(/* what paremeters do we need? */) {
+function orderPizza(var topping = document.getElementById('toppings').value) {
+	console.log(topping);
   // let's add our code here to handle ordering a pizza
-  
+
   // don't forget to stop the form from reloading the page
+  return false;
 }
 
 // add a click handler for the #selectToppings button
 // add a click handler for the #addmore button
 
-function placeOrder(/* what parameters do we need? */) {
+function placeOrder( ) {
   // show the 'addmore' and 'done' and console.log a message that talks about the dog.
   // add the dog message to the array
 }
