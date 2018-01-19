8668a0806d955e06d441530b85c3d0bba4d90933
bseiner <brseiner@gmail.com>
2018-01-18 21:03:52 -0500

    init commit

---
+++ jsr-hw-4/js/index.js
@@ -0,0 +1,22 @@
+var cheeses = ["provolone", "mozz"];
+var names = ["Jess", "Kat", "Molly", "Bailey"];
+var dogs = [];
+
+function orderPizza(/* what paremeters do we need? */) {
+  // let's add our code here to handle adopting a dog
+  // don't forget to stop the form from reloading the page
+}
+
+// add a click handler for the #selectToppings button
+// add a click handler for the #addmore button
+
+function placeOrder(/* what parameters do we need? */) {
+  // show the 'addmore' and 'done' and console.log a message that talks about the dog.
+  // add the dog message to the array
+}
+
+function finishHandler() {
+  // list out all orders
+}
+
+// add a click handler for the #done button
